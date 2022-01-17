d71479e9e5205a8fa3e43e708096c7ffa93e2d6a
Ely Karr <elykarr@gmail.com>
2022-01-17 14:01:41 -0800

    commit tree 1754cadc0e1e5f8a26f52d03d388a6640b6d30a4

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,19 @@
-# Make your shoe class here!
+require 'pry'
+
+class Shoe
+
+    attr_accessor :brand, :color, :size, :material, :condition
+
+    def initialize(brand)
+        @brand = brand
+    end
+
+    def cobble
+        @condition= "new"
+        # if self.condition == "old"
+        #     self.condition= "new"
+        # end
+        puts "Your shoe is as good as new!"
+    end
+
+end
